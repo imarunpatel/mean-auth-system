0e7162f5b7dc664b57614e2603f50b54dfdfe9e9
Arun <arunpatel209@gmail.com>
2020-08-30 09:25:50 +0530

    app name modified

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/Mean-Auth-System'));
+app.use(express.static(__dirname + '/dist/my-first-app'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
-        '/dist/Mean-Auth-System/index.html'));
+        '/dist/my-first-app/index.html'));
 });
 app.listen(process.env.PORT || 8080)
